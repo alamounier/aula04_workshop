ab0d57718aed427174e7374b07d0cd4d5b090eaa
André Moraes <andrelamounier@gmail.com>
2024-07-02 16:35:33 -0300

    atualizando arquivos

--- src/frontend.py
+++ src/frontend.py
@@ -14,3 +14,10 @@ class ExcelValidorUI:
     
     def upload_file(self):
         return st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
+    
+    def display_results(self, result, errors):
+        if errors:
+            for error in errors:
+                st.error(f"Erro na validação: {error}")
+        else:
+            st.success("O schema do arquivo Excel está correto")
--- tests/test_funcional.py
+++ tests/test_funcional.py
@@ -3,7 +3,6 @@ from time import sleep
 import pytest
 import subprocess 
 from selenium.webdriver.common.by import By 
-import os 
 from selenium.webdriver.firefox.options import Options 
 
 @pytest.fixture 
@@ -26,23 +25,23 @@ def test_app_opens(driver):
     driver.get("http://localhost:8501")
     sleep(2)
 
-# def test_check_title_is(driver):
-#     # Verificar se a página abre
-#     driver.get("http://localhost:8501")
-#     # Verificar se o título da página é
-#     sleep(2)
-#     page_title = driver.title
+def test_check_title_is(driver):
+    # Verificar se a página abre
+    driver.get("http://localhost:8501")
+    # Verificar se o título da página é
+    sleep(2)
+    page_title = driver.title
 
-#     # Verificar se o título da página é o esperado
-#     expected_title = "Validador de schema excel" 
-#     assert page_title == expected_title
+    # Verificar se o título da página é o esperado
+    expected_title = "Validador de schema excel" 
+    assert page_title == expected_title
 
-# def test_check_streamlit_h1(driver):
-#     # Acessar a página do Streamlit
-#     driver.get("http://localhost:8501")
-#     sleep(2)
-#     # Captura o primeiro elemento <h1> da página
-#     h1_element = driver.find_element(By.TAG_NAME, "h1")
-#     # Verifica se o texto do elemento <h1> é o esperado
-#     expected_text = "Insira o seu excel para validação"
-#     assert h1_element.text == expected_text
+def test_check_streamlit_h1(driver):
+    # Acessar a página do Streamlit
+    driver.get("http://localhost:8501")
+    sleep(2)
+    # Captura o primeiro elemento <h1> da página
+    h1_element = driver.find_element(By.TAG_NAME, "h1")
+    # Verifica se o texto do elemento <h1> é o esperado
+    expected_text = "Insira o seu excel para validação"
+    assert h1_element.text == expected_text
