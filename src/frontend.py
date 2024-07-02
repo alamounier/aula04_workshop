961db6711614fde7b1d362e705db260b5dd16412
André Moraes <andrelamounier@gmail.com>
2024-07-02 16:58:07 -0300

    teste

--- src/frontend.py
+++ src/frontend.py
@@ -10,7 +10,7 @@ class ExcelValidorUI:
         ) 
 
     def display_header(self):
-        st.title("Validador de schema excel")
+        st.title("teste")
     
     def upload_file(self):
         return st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
